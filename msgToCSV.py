0eb4c423e4ee50cc30d3f6e6acd3243108663ecc
dada97 <kerodada@gmail.com>
2022-06-29 17:11:54 +0800

    change msgtocsv library

--- msgToCSV.py
+++ msgToCSV.py
@@ -1,13 +1,9 @@
 import os
 import sys
 import msgpack
-import json
-import io
 import numpy as np
 
-
-from autolab_core import RigidTransform
-
+from scipy.spatial.transform import Rotation as R
 def main(bin_fn, dest_fn):
 
     output = os.path.join(dest_fn, "slamData.csv");
@@ -23,20 +19,26 @@ def main(bin_fn, dest_fn):
 
     key_frame = {int(k): v for k, v in key_frames.items()}
 
-  
     with open(output, "w") as f:
-        f.write("key_frame_id,pos_x,pos_y,pos_z,rot_x,rot_y,rot_z,rot_w,timestamp\n")
+        f.write(",key_frame_id,pos_x,pos_y,pos_z,rot_x,rot_y,rot_z,rot_w,timestamp\n")
+        count = 0;
         for key in sorted(key_frame.keys()):
-            
+            count +=1
             point = key_frame[key]
             #print(point[])
-            trans_cw = np.asarray(point["trans_cw"])
+
             rot_cw = np.asarray(point["rot_cw"])
             ts = point["ts"] 
-            rigid_cw = RigidTransform(rot_cw, trans_cw)
-            pos = np.matmul(rigid_cw.rotation, trans_cw)
 
-            f.write("{},{},{},{},{},{},{},{},{}\n".format(key,pos[0], pos[1], pos[2],rot_cw[0],rot_cw[1],rot_cw[2],rot_cw[3],ts),)
+            # get conversion from camera to world
+            trans_cw = np.matrix(point["trans_cw"]).T
+            rot = R.from_quat(point["rot_cw"]).as_matrix()
+            
+            # compute conversion from world to camera
+            rot_wc = rot.T
+            trans_wc = - rot_wc * trans_cw
+
+            f.write("{},{},{},{},{},{},{},{},{},{}\n".format(count,key,trans_wc[0, 0], trans_wc[1, 0],trans_wc[2, 0],rot_cw[0],rot_cw[1],rot_cw[2],rot_cw[3],ts),)
     print("Done")
 
 if __name__ == "__main__":
